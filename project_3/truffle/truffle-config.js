4a0895cee7024f97429abfe71dacccb89a6a9196
pourcharly <pourcharly@hotmail.com>
2022-11-22 03:56:45 +0100

    truffle unbox react on project 3

---
+++ project_3/truffle/truffle-config.js
@@ -0,0 +1,39 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider'); 
+const { MNEMONIC, INFURA_ID } = require('dotenv').config();
+
+module.exports = {
+  contracts_build_directory: "../client/src/contracts",
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      websocket: true,
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${INFURA_ID}`),
+      network_id: 5,       // Goerli's id
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {},
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.13", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        //  evmVersion: "byzantium"
+      }
+    }
+  }
+};
